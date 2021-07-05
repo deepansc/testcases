a6d9ec7b1c447e279b89c780f4a84cedbc3a89b6
deepanshu <deepanshu@DESKTOP-NB5LOQ6>
2021-07-05 12:00:10 +0530

    initial comment added

---
+++ src/com/cg/ppmdemo/dao/Project.java
@@ -0,0 +1,19 @@
+package com.cg.ppmdemo.dao;
+
+public class Project {
+private String name;
+private int id;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+
+}
